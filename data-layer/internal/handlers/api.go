e4635b2f415e54339509d2221327599543d443e6
Amine Afia <amine@thirdweb.com>
2024-09-17 12:38:27 +0200

    Initial project structure, API schema, auth, DB connector (#1)
    * initial persistenc einterface
    
    * added api stripped api & basic auth
    
    * added DB read

---
+++ data-layer/internal/handlers/api.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	chimiddle "github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/thirdweb-dev/data-layer/internal/middleware"
+)
+
+func Handler(r *chi.Mux) {
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/api", func(router chi.Router) {
+		router.Use(middleware.Authorization)
+		router.Route("/v1", func(r chi.Router) {
+			r.Get("/blocks", GetBlocks)
+		})
+	})
+}
---
+++ data-layer/internal/handlers/get_blocks.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/schema"
+	log "github.com/sirupsen/logrus"
+	"github.com/thirdweb-dev/data-layer/api"
+	"github.com/thirdweb-dev/data-layer/internal/tools"
+)
+
+func GetBlocks(w http.ResponseWriter, r *http.Request) {
+	var params = api.QueryParams{}
+	var decoder *schema.Decoder = schema.NewDecoder()
+	var err error
+
+	err = decoder.Decode(&params, r.URL.Query())
+
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+
+	conn, err := tools.ConnectDB()
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+	defer conn.Close()
+
+	row := conn.QueryRow(context.Background(), "SELECT block_hash FROM chainsaw.blocks LIMIT 1")
+	var blockHash string
+    err = row.Scan(&blockHash)
+	fmt.Printf("valoue %v\n", blockHash)
+    if err != nil {
+        log.Error(err)
+        api.InternalErrorHandler(w)
+        return
+    }
+
+    var response = api.QueryResponse{
+        Result: blockHash,
+        Code:   http.StatusOK,
+    }
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+}
