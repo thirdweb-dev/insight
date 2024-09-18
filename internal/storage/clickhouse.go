63889a91094e23d9d57901c87730bec8b281e7d4
Amine Afia <amine@thirdweb.com>
2024-09-18 11:39:45 +0200

    Implemented Modular Storage interface (#11)
    * Added Clickhouse Storage Connector
    
    * improved clickhouse connector for readability (#15)
    
    ### TL;DR
    
    Refactored ClickHouseConnector struct and improved database connection handling.
    
    ### What changed?
    
    - Renamed `cache` field to `conn` in ClickHouseConnector struct for clarity
    - Added `Database` field to ClickhouseConnectorConfig struct
    - Updated method calls to use `conn` instead of `cache`
    - Adjusted error handling in the Get method
    
    ### How to test?
    
    1. Ensure all existing tests pass with the updated code
    2. Verify that database connections are established correctly using the new configuration
    3. Test CRUD operations (Get, Set, Delete) to confirm they work as expected with the renamed `conn` field
    
    ### Why make this change?
    
    This refactoring improves code readability and maintainability by using more accurate naming conventions. The addition of the `Database` field in the configuration allows for more flexible database connections. These changes enhance the overall structure and functionality of the ClickHouseConnector, making it easier to work with and extend in the future.
    
    * Implement IStorage Factory to interact with cache and DB connectors (#14)
    
    ### TL;DR
    
    Refactored storage interfaces and implemented placeholder methods for ClickHouse connector.
    
    ### What changed?
    
    - Introduced new interfaces `IStorage`, `IStorageBase`, `IStorageCache`, and `IStorageDB` in `connector.go`.
    - Updated `NewStorageConnector` function to return `IStorage` instead of `StorageConnector`.
    - Added placeholder methods for ClickHouse connector in `clickhouse.go`: `query`, `insert`, `delete`, `queryCache`, `purgeCache`, and `setCache`.
    - Renamed methods in `memory.go` to align with new interface: `Set` to `setCache`, `Get` to `queryCache`, and `Delete` to `purgeCache`.
    
    ### How to test?
    
    1. Ensure all existing tests pass after the refactoring.
    2. Verify that the memory connector still functions correctly with the renamed methods.
    3. Check that the ClickHouse connector can be initialized without errors.
    4. Attempt to use the new placeholder methods for the ClickHouse connector and confirm they return expected "not implemented" results.
    
    ### Why make this change?
    
    This refactoring aims to create a more flexible and consistent interface for different storage connectors. By separating concerns into distinct interfaces (base, cache, and DB operations), it allows for easier implementation of new storage solutions and better organization of existing ones. The placeholder methods for ClickHouse set the groundwork for future implementation of these operations.

---
+++ internal/storage/clickhouse.go
@@ -0,0 +1,122 @@
+package storage
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+type ClickHouseConnector struct {
+	conn clickhouse.Conn
+}
+
+type ClickhouseConnectorConfig struct {
+	ExpiresAt time.Duration
+	Database string
+}
+
+func NewClickHouseConnector(cfg *ClickhouseConnectorConfig) (*ClickHouseConnector, error) {
+	conn, err := ConnectDB()
+	// Question: Should we add the table setup here?
+	if err != nil {
+		return nil, err
+	}
+	return &ClickHouseConnector{
+		conn:  conn,
+	}, nil
+}
+
+
+func (c *ClickHouseConnector) Get(index, partitionKey, rangeKey string) (string, error) {
+	key := fmt.Sprintf("%s:%s", partitionKey, rangeKey)
+	// Does it make sense to check the expiration duration in the query?
+	query := "SELECT value FROM chainsaw.indexer_cache WHERE key = ?"
+	var value string
+    err := c.conn.QueryRow(context.Background(), query, key).Scan(&value)
+    if err != nil {
+		log.Error(err.Error())
+		return "", fmt.Errorf("record not found for key: %s", key)
+	}
+	return value, nil
+}
+
+func (c *ClickHouseConnector) Set(partitionKey, rangeKey, value string) error {
+	key := fmt.Sprintf("%s:%s", partitionKey, rangeKey)
+	query := "INSERT INTO chainsaw.indexer_cache (key, value, expires_at) VALUES (?, ?, ?)"
+	err := c.conn.Exec(
+		context.Background(),
+		query,
+		key, value, time.Now().Add(time.Hour),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ClickHouseConnector) Delete(index, partitionKey, rangeKey string) error {
+	key := fmt.Sprintf("%s:%s", partitionKey, rangeKey)
+	query := fmt.Sprintf("DELETE FROM chainsaw.cache WHERE key = %s", key)
+	err := c.conn.Exec(context.Background(), query)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ConnectDB() (clickhouse.Conn, error) {
+	port, err := strconv.Atoi(os.Getenv("CLICKHOUSE_PORT"))
+	if err != nil {
+		return nil, fmt.Errorf("invalid CLICKHOUSE_PORT: %w", err)
+	}
+
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{fmt.Sprintf("%s:%d", os.Getenv("CLICKHOUSE_HOST"), port)},
+		Protocol: clickhouse.Native,
+		TLS: &tls.Config{}, // enable secure TLS
+		Auth: clickhouse.Auth{
+			Username: os.Getenv("CLICKHOUSE_USERNAME"),
+			Password: os.Getenv("CLICKHOUSE_PASSWORD"),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func (c *ClickHouseConnector) query(query string) (string, error) {
+	// not implemented yet
+	return "", nil
+}
+
+func (c *ClickHouseConnector) insert(query string) error {
+	// not implemented yet
+	return nil
+}
+
+func (c *ClickHouseConnector) delete(query string) error {
+	// not implemented yet
+	return nil
+}
+
+func (c *ClickHouseConnector) queryCache(index, partitionKey, rangeKey string) (string, error) {
+	// not implemented yet
+	return "", nil
+}
+
+func (c *ClickHouseConnector) purgeCache(index, partitionKey, rangeKey string) error {
+	// not implemented yet
+	return nil
+}
+
+func (c *ClickHouseConnector) setCache(partitionKey, rangeKey, value string) error {
+	// not implemented yet
+	return nil
+}
