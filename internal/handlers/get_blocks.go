700e72c01b04935f29ebe5373a1208c4022505d8
Amine Afia <amine@thirdweb.com>
2024-09-19 16:47:49 +0300

    clickhouse-storage

--- internal/handlers/get_blocks.go
+++ internal/handlers/get_blocks.go
@@ -1,16 +1,84 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/gorilla/schema"
 	log "github.com/sirupsen/logrus"
 	"github.com/thirdweb-dev/indexer/api"
-	"github.com/thirdweb-dev/indexer/internal/storage"
+	"github.com/thirdweb-dev/indexer/internal/common"
 )
+
+func CreateMockBlock() *types.Block {
+	// header := &types.Header{
+	// 	ParentHash:  common.HexToHash("0x1234567890abcdef"),
+	// 	UncleHash:   common.HexToHash("0xabcdef1234567890"),
+	// 	Coinbase:    common.HexToAddress("0x1234567890123456789012345678901234567890"),
+	// 	Root:        common.HexToHash("0x9876543210fedcba"),
+	// 	TxHash:      common.HexToHash("0xfedcba9876543210"),
+	// 	ReceiptHash: common.HexToHash("0x0123456789abcdef"),
+	// 	Bloom:       types.Bloom{},
+	// 	Difficulty:  big.NewInt(1000000),
+	// 	Number:      big.NewInt(12345),
+	// 	GasLimit:    1000000,
+	// 	GasUsed:     500000,
+	// 	Time:        uint64(time.Now().Unix()),
+	// 	Extra:       []byte("Mock block"),
+	// }
+	// body := &types.Body{
+	// 	Transactions: []*types.Transaction{
+	// 		{},  // Add mock transactions if needed
+	// 	},
+	// 	Uncles: []*types.Header{
+	// 		{},  // Add mock uncle headers if needed
+	// 	},
+	// }
+
+	// receipts := []*types.Receipt{
+	// 	{},  // Add mock receipts if needed
+	// }
+
+	return types.NewBlock(nil, nil, nil, nil)
+}
+
+func fetchBlock() (string, error) {
+	url := "https://1.rpc.thirdweb.com"
+	payload := strings.NewReader(`{"id":1,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",true]}`)
+
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to send request: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
+	}
+	// TODO: need to serialize the block to be proccessed
+
+	var blockResponse struct {
+		Result string `json:"result"`
+	}
+	if err := json.Unmarshal(body, &blockResponse); err != nil {
+		return "", fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+	fmt.Println(blockResponse.Result)
+	return blockResponse.Result, nil
+}
 
 func GetBlocks(w http.ResponseWriter, r *http.Request) {
 	var params = api.QueryParams{}
@@ -24,27 +92,16 @@ func GetBlocks(w http.ResponseWriter, r *http.Request) {
 		api.InternalErrorHandler(w)
 		return
 	}
-
-	conn, err := storage.ConnectDB()
+	block, err := fetchBlock()
 	if err != nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
 		return
 	}
-
-	row := conn.QueryRow(context.Background(), "SELECT block_number FROM chainsaw.blocks LIMIT 1")
-	var blockNumber uint64
-    err = row.Scan(&blockNumber)
-    if err != nil {
-        log.Error(err)
-        api.RequestErrorHandler(w, err)
-        return
-    }
-
-    var response = api.QueryResponse{
-        Result: fmt.Sprintf("%d", blockNumber),
-        Code:   http.StatusOK,
-    }
+	var response = api.QueryResponse{
+		Result: fmt.Sprintf("%v", block),
+		Code:   http.StatusOK,
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(response)
@@ -53,6 +110,4 @@ func GetBlocks(w http.ResponseWriter, r *http.Request) {
 		api.InternalErrorHandler(w)
 		return
 	}
-
-	defer conn.Close()
 }
--- internal/storage/clickhouse.go
+++ internal/storage/clickhouse.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/thirdweb-dev/indexer/internal/common"
 )
@@ -36,7 +37,7 @@ func NewClickHouseConnector(cfg *ClickhouseConnectorConfig) (*ClickHouseConnecto
 func (c *ClickHouseConnector) Get(index, partitionKey, rangeKey string) (string, error) {
 	key := fmt.Sprintf("%s:%s", partitionKey, rangeKey)
 	// Does it make sense to check the expiration duration in the query?
-	query := "SELECT value FROM chainsaw.indexer_cache WHERE key = ?"
+	query := "SELECT value FROM base.indexer_cache WHERE key = ?"
 	var value string
 	err := c.conn.QueryRow(context.Background(), query, key).Scan(&value)
 	if err != nil {
@@ -48,7 +49,7 @@ func (c *ClickHouseConnector) Get(index, partitionKey, rangeKey string) (string,
 
 func (c *ClickHouseConnector) Set(partitionKey, rangeKey, value string) error {
 	key := fmt.Sprintf("%s:%s", partitionKey, rangeKey)
-	query := "INSERT INTO chainsaw.indexer_cache (key, value, expires_at) VALUES (?, ?, ?)"
+	query := "INSERT INTO base.indexer_cache (key, value, expires_at) VALUES (?, ?, ?)"
 	err := c.conn.Exec(
 		context.Background(),
 		query,
@@ -62,7 +63,7 @@ func (c *ClickHouseConnector) Set(partitionKey, rangeKey, value string) error {
 
 func (c *ClickHouseConnector) Delete(index, partitionKey, rangeKey string) error {
 	key := fmt.Sprintf("%s:%s", partitionKey, rangeKey)
-	query := fmt.Sprintf("DELETE FROM chainsaw.cache WHERE key = %s", key)
+	query := fmt.Sprintf("DELETE FROM base.cache WHERE key = %s", key)
 	err := c.conn.Exec(context.Background(), query)
 	if err != nil {
 		return err
@@ -91,23 +92,100 @@ func connectDB() (clickhouse.Conn, error) {
 	return conn, nil
 }
 
-func (c *ClickHouseConnector) InsertBlocks(blocks []common.Block) error {
-	return nil
+func (c *ClickHouseConnector) InsertBlocks(blocks []types.Block) error {
+	batch, err := c.conn.PrepareBatch(context.Background(), "INSERT INTO base.blocks (number, hash, parent_hash, nonce, mix_digest, uncle_hash, extra, difficulty, size, gas_limit, gas_used, time) VALUES")
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(blocks); i++ {
+		err := batch.Append(
+			blocks[i].Number,
+			blocks[i].Hash,
+			blocks[i].ParentHash,
+			blocks[i].Nonce,
+			blocks[i].MixDigest,
+			blocks[i].UncleHash,
+			blocks[i].Extra,
+			blocks[i].Difficulty,
+			blocks[i].Size,
+			blocks[i].GasLimit,
+			blocks[i].GasUsed,
+			blocks[i].Time,
+		)
+		if err != nil {
+			log.Error(err.Error())
+			return err
+		}
+	}
+	return batch.Send()
 }
 
-func (c *ClickHouseConnector) InsertTransactions(txs []common.Transaction) error {
-	return nil
+func (c *ClickHouseConnector) InsertTransactions(txs []types.Transaction) error {
+	batch, err := c.conn.PrepareBatch(context.Background(), "INSERT INTO base.transactions (hash, block_number, index, from_address, to_address, value, gas, gas_price, input, nonce, transaction_index, max_fee_per_gas, max_priority_fee_per_gas, transaction_type, access_list, chain_id, v, r, s) VALUES")
+	if err != nil {
+		return err
+	}
+	// for i := 0; i < len(txs); i++ {
+	// 	err := batch.Append(
+	// 		txs[i].Hash(),
+	// 		txs[i].inner.BlockNumber,
+	// 		txs[i].inner.TransactionIndex,
+	// 		txs[i].inner.From,
+	// 		txs[i].inner.To,
+	// 		txs[i].inner.Value,
+	// 		txs[i].inner.Gas,
+	// 		txs[i].inner.GasPrice,
+	// 		txs[i].inner.Input,
+	// 	)
+	// 	if err != nil {
+	// 		log.Error(err)
+	// 		return err
+	// 	}
+	// }
+	return batch.Send()
 }
 
-func (c *ClickHouseConnector) InsertEvents(events []common.Log) error {
-	return nil
+func (c *ClickHouseConnector) InsertEvents(events []types.Log) error {
+	batch, err := c.conn.PrepareBatch(context.Background(), "INSERT INTO base.events (block_number, index, address, topics, data, log_index) VALUES")
+	if err != nil {
+		return err
+	}
+	// for i := 0; i < len(events); i++ {
+	// 	err := batch.Append(
+	// 		events[i].BlockNumber,
+	// 		events[i].Index,
+	// 		events[i].Address,
+	// 		events[i].Topics,
+	// 		events[i].Data,
+	// 		events[i].LogIndex,
+	// 	)
+	// 	if err != nil {
+	// 		log.Error(err)
+	// 		return err
+	// 	}
+	// }
+	return batch.Send()
 }
 
-func (c *ClickHouseConnector) GetBlocks(limit int) (events []common.Block, err error) {
-	return nil, nil
+func (c *ClickHouseConnector) GetBlocks(limit int) (events []*types.Block, err error) {
+	query := "SELECT * FROM base.blocks ORDER BY number DESC LIMIT ?"
+	rows, err := c.conn.Query(context.Background(), query, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var blocks []*types.Block
+	for rows.Next() {
+		var block types.Block
+		if err := rows.Scan(&block); err != nil {
+			return nil, err
+		}
+		blocks = append(blocks, &block)
+	}
+	return blocks, nil
 }
 
-func (c *ClickHouseConnector) GetTransactions(blockNumber uint64, limit int) (events []common.Transaction, err error) {
+func (c *ClickHouseConnector) GetTransactions(blockNumber uint64, limit int) (transactions []*types.Transaction, err error) {
 	return nil, nil
 }
 
