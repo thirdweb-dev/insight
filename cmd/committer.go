1dfc50a94f8bb029da4dca745a2d1f4b6267dd39
nischit <nischitpra@gmail.com>
2025-09-23 21:33:19 +0545

    committer metrics server

--- cmd/committer.go
+++ cmd/committer.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/thirdweb-dev/indexer/internal/committer"
 )
@@ -16,6 +19,16 @@ var committerCmd = &cobra.Command{
 
 func RunCommitter(cmd *cobra.Command, args []string) {
 	fmt.Println("running committer")
+
+	// Start Prometheus metrics server
+	log.Info().Msg("Starting Metrics Server on port 2112")
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(":2112", nil); err != nil {
+			log.Error().Err(err).Msg("Metrics server error")
+		}
+	}()
+
 	committer.Init()
 	committer.CommitStreaming()
 }
