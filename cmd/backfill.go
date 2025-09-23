2bf90e08e52193c7e78afa4e87b6ffc8e3f29963
nischit <nischitpra@gmail.com>
2025-09-23 21:11:11 +0545

    metrics server for backfill

--- cmd/backfill.go
+++ cmd/backfill.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/thirdweb-dev/indexer/internal/backfill"
 )
@@ -16,6 +19,16 @@ var backfillCmd = &cobra.Command{
 
 func RunBackfill(cmd *cobra.Command, args []string) {
 	fmt.Println("running backfill")
+
+	// Start Prometheus metrics server
+	log.Info().Msg("Starting Metrics Server on port 2112")
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(":2112", nil); err != nil {
+			log.Error().Err(err).Msg("Metrics server error")
+		}
+	}()
+
 	backfill.Init()
 	backfill.RunBackfill()
 }
