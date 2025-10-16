a50b9b67b9dca30f9abdf105c4574238281d582d
nischit <nischitpra@gmail.com>
2025-10-17 02:54:53 +0545

    minor change

--- configs/config.go
+++ configs/config.go
@@ -302,11 +302,10 @@ type Config struct {
 	ParquetMaxFileSizeMB             int64  `env:"PARQUET_MAX_FILE_SIZE_MB" envDefault:"512"`
 	InsightServiceUrl                string `env:"INSIGHT_SERVICE_URL" envDefault:"https://insight.thirdweb.com"`
 	InsightServiceApiKey             string `env:"INSIGHT_SERVICE_API_KEY"`
-	RedisAddr                        string `env:"REDIS_ADDR"`
+	RedisAddr                        string `env:"REDIS_ADDR" envDefault:"localhost:6379"`
 	RedisUsername                    string `env:"REDIS_USERNAME"`
 	RedisPassword                    string `env:"REDIS_PASSWORD"`
-	RedisDB                          int    `env:"REDIS_DB"`
-	RedisEnableTLS                   bool   `env:"REDIS_ENABLE_TLS" envDefault:"true"`
+	RedisDB                          int    `env:"REDIS_DB" envDefault:"0"`
 }
 
 var Cfg Config
